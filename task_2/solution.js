3182775d4a9546549a317b18ea6cfeb082c11509
mikapoly <m.belov@polymatica.com>
2022-09-27 22:46:09 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,7 +2,13 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
+    let shippingSum = sum;
 
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    } else if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+        shippingSum = shippingPrice;
+    }
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
@@ -25,6 +31,10 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
+    let discountSum = sum;
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart / 100 * productsSum;
+    } else discountSum = 0;
 
     // Задание №2.2. Рассчитать скидку
 
@@ -42,6 +52,10 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum = sum;
+    totalSum = productsSum;
+    totalSum = (totalSum - discountSum);
+
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -51,7 +65,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+totalSum = (totalSum + shippingSum);
 
+let freeShipping;
+    freeShipping = !Boolean(shippingSum);
+
+    
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
